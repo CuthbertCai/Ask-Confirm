f4ac76dda615d4bda792ad3847761fd20266b800
cgy <caiguanyu1995@outlook.com>
2021-11-28 13:12:34 +0800

    update scripts/train_image_attribute.sh

--- scripts/train_image_attribute.sh
+++ scripts/train_image_attribute.sh
@@ -3,7 +3,7 @@ python train/train_img_attr.py \
        --num_workers=1 \
        --n_feature_dim=256 \
        --focal_loss=False \
-       --n_categories=501 \
+       --n_categories=1601 \
        --n_epochs=50 \
        --lr=1e-5 \
        --exp_name=image_attribute
